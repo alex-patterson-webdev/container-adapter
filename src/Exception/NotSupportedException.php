00ee1c478f0daca5c006944e5b9c9fd428b027fb
Alex Patterson <alex.patterson@eagleeye.com>
2019-09-25 14:24:53 +0100

    Add new exception NotSupportedException to allow adapters to throw errors if they do not support the relevent adapter methods.

---
+++ src/Exception/NotSupportedException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Arp\Container\Exception;
+
+/**
+ * NotSupportedException
+ *
+ * Exception used for adapter methods that are not supported on the wrapped container.
+ *
+ * @package Arp\Container\Exception
+ */
+class NotSupportedException extends ContainerException
+{}
