44b08bdfac48501c7bcf3a0e5f58f7b4557d1f67
Alex Patterson <alex.patterson.webdev@gmail.com>
2020-03-22 16:00:15 +0000

    Add new AliasAwareInterface to allow adapters to define alias to already registered services

---
+++ src/Adapter/AliasAwareInterface.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Arp\Container\Adapter;
+
+use Arp\Container\Adapter\Exception\AdapterException;
+
+/**
+ * @author  Alex Patterson <alex.patterson.webdev@gmail.com>
+ * @package Arp\Container\Adapter
+ */
+interface AliasAwareInterface
+{
+    /**
+     * Set an alias for a given service
+     *
+     * @param string $alias The name of the alias to set
+     * @param string $name The name of the service that
+     *
+     * @return ContainerAdapterInterface
+     *
+     * @throws  AdapterException If the alias cannot be set
+     */
+    public function setAlias(string $alias, string $name): ContainerAdapterInterface;
+}
