cd51192dcf90f4a027e50e09efe238f58ce34668
Alex Patterson <alex.patterson.webdev@gmail.com>
2020-09-06 10:19:08 +0000

    Added new InvaliArgumentException class that extends the default ContainerException

--- src/Exception/InvalidArgumentException.php
+++ src/Exception/InvalidArgumentException.php
@@ -4,13 +4,11 @@ declare(strict_types=1);
 
 namespace Arp\Container\Exception;
 
-use Psr\Container\NotFoundExceptionInterface;
-
 /**
  * @author  Alex Patterson <alex.patterson.webdev@gmail.com>
  * @package Arp\Container\Exception
  */
-final class NotFoundException extends ContainerException implements NotFoundExceptionInterface
+final class InvalidArgumentException extends ContainerException
 {
 
 }
